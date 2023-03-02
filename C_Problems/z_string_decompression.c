60b23e8a2073d7e112ab54e7728fc08f10e09bf1
ramesh6859 <ramesh6859@gmail.com>
2023-03-02 16:55:01 +0530

    Added SDLC

--- C_Problems/z_string_decompression.c
+++ C_Problems/z_string_decompression.c
@@ -1,52 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#define MAX_LEN 100
-
-void expandString(char *inputString, char *outputString);
-
-int main()
+char *expandString(char *input)
 {
-    char inputString[MAX_LEN] = "abc2[pq]";
-    char outputString[MAX_LEN];
-
-    expandString(inputString, outputString);
-
-    printf("Input String: %s\n", inputString);
-    printf("Output String: %s\n", outputString);
-
-    return 0;
-}
-
-void expandString(char *inputString, char *outputString)
-{
-    int inputLength = strlen(inputString);
-    int outputLength = 0;
-
-    for (int i = 0; i < inputLength; i++)
+    int input_len = strlen(input);
+    char *output = malloc(input_len * sizeof(char));
+    int output_index = 0;
+    char current_char;
+    int repeat_count;
+    for (int i = 0; i < input_len; i++)
     {
-        if (inputString[i] >= '0' && inputString[i] <= '9')
+        printf("%c ------", input[i]);
+        current_char = input[i];
+        if (current_char == '[')
         {
-            int num = inputString[i] - '0';
-            for (int j = 0; j < num; j++)
+            repeat_count = input[i - 1] - '0';
+            for (int j = 0; j < repeat_count; j++)
             {
-                for (int k = i + 1; k < inputLength; k++)
-                {
-                    if (inputString[k] == ']')
-                    {
-                        break;
-                    }
-                    outputString[outputLength++] = inputString[k];
-                }
+                output[output_index++] = input[i + 1];
             }
-            i++; // skip the closing bracket
+            while (input[i] != ']')
+            {
+                i++;
+            }
         }
         else
         {
-            outputString[outputLength++] = inputString[i];
+            output[output_index++] = current_char;
         }
     }
-
-    outputString[outputLength] = '\0';
+    output[output_index] = '\0';
+    // for (int l = 0; l < sizeof(output); l++)
+    // {
+    //     printf("%c ", output[l]);
+    // }
+    return output;
+}
+int main()
+{
+    char *input1 = "a3[b]c";
+    char *output1 = expandString(input1);
+    printf("Input: %s\n", input1);
+    printf("Output: %s\n", output1);
+    free(output1);
+    char *input2 = "abc2[pq]";
+    char *output2 = expandString(input2);
+    printf("Input: %s\n", input2);
+    printf("Output: %s\n", output2);
+    free(output2);
+    return 0;
 }
