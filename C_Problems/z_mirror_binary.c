8341b348b68414fe2e03576943a717288cca2392
ramesh6859 <ramesh6859@gmail.com>
2023-03-07 20:51:12 +0530

    PWM Watched

--- C_Problems/z_mirror_binary.c
+++ C_Problems/z_mirror_binary.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
-
-int main() {
+int main()
+{
     int decimal, mirrored_decimal = 0;
-
     printf("Enter a decimal number: ");
     scanf("%d", &decimal);
-
-    // Calculate the mirrored binary representation using bit-shifting
-    int num_bits = sizeof(decimal) * 8;  // Calculate the number of bits in an integer
-    for (int i = 0; i < num_bits; i++) {
-        if (decimal & (1 << i)) {  // Check if the i-th bit is set
-            mirrored_decimal |= 1 << (num_bits - 1 - i);  // Set the corresponding mirrored bit
+    int num_bits = sizeof(decimal) * 8;
+    for (int i = 0; i < num_bits; i++)
+    {
+        if (decimal & (1 << i))
+        {
+            mirrored_decimal |= 1 << (num_bits - 1 - i);
         }
     }
-
     printf("The mirrored binary representation of the decimal number is: %d\n", mirrored_decimal);
-
     return 0;
 }
