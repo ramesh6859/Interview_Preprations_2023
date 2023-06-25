c5e937ecf35ffd50756dfad641c838e208c7aa11
ramesh6859 <ramesh6859@gmail.com>
2023-06-25 23:22:12 +0530

    Updated before KPIT joining

--- C_Problems/Others/Array_Second_Largest_One_For_Loop.c
+++ C_Problems/Others/Array_Second_Largest_One_For_Loop.c
@@ -1,21 +1,51 @@
+// #include <stdio.h>
+// int main()
+// {
+//     system("cls");
+//     int arr[] = {5, 8, 2, 10, 3};
+//     int size = sizeof(arr) / sizeof(arr[0]);
+//     int largest = arr[0];
+//     int secondLargest = arr[0];
+//     for (int i = 1; i < size; i++)
+//     {
+//         if (arr[i] > largest)
+//         {
+//             secondLargest = largest;
+//             largest = arr[i];
+//         }
+//         else if (arr[i] > secondLargest && arr[i] != largest)
+//             secondLargest = arr[i];
+//     }
+//     printf("The second largest element is: %d\n", secondLargest);
+//     return 0;
+// }
+
+//  ############################################################
+
+//
 #include <stdio.h>
+int findSecondLargest(int *, int);
 int main()
 {
-    system("cls");
-    int arr[] = {5, 8, 2, 10, 3};
+    int arr[] = {9, 5, 2, 8, 1, 10, 7};
     int size = sizeof(arr) / sizeof(arr[0]);
-    int largest = arr[0];
-    int secondLargest = arr[0];
+    int secondLargest = findSecondLargest(arr, size);
+    printf("Second largest element: %d\n", secondLargest);
+    return 0;
+}
+int findSecondLargest(int *arr, int size)
+{
+    int largest = *arr;
+    int secondLargest = *arr;
     for (int i = 1; i < size; i++)
     {
-        if (arr[i] > largest)
+        if (*(arr + i) > largest) 
         {
             secondLargest = largest;
-            largest = arr[i];
-        }
-        else if (arr[i] > secondLargest && arr[i] != largest)
-            secondLargest = arr[i];
+            largest = *(arr + i);
+        } 
+        else if (*(arr + i) > secondLargest && *(arr + i) < largest) 
+            secondLargest = *(arr + i);
     }
-    printf("The second largest element is: %d\n", secondLargest);
-    return 0;
+    return secondLargest;
 }
